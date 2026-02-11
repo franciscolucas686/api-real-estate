a4a20c260b92d4d68a7ba947a28e751a87624125
Francisco Lucas <franciscolucas686@gmail.com>
2026-02-11 06:12:19 -0300

    :sparkles:feat: implement into AppModule cache and exceptions error

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { CacheInterceptor } from './common/interceptors/cache.interceptor';
+import { AppConfigModule } from './config/config.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PropertiesModule } from './properties/properties.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    AppConfigModule,
     PrismaModule,
     AuthModule,
     UsersModule,
@@ -21,6 +25,20 @@ import { UsersModule } from './users/users.module';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
